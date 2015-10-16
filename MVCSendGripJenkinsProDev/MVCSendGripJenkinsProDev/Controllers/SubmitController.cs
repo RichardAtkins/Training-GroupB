c4347f03955cef0c7b5644246286a636e4487a30
RichardAtkins <RichardA@nVisionIT.co.za>
2015-10-16 14:54:54 +0200

    added first test

---
+++ MVCSendGripJenkinsProDev/MVCSendGripJenkinsProDev/Controllers/SubmitController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace MVCSendGripJenkinsProDev.Controllers
+{
+    public class SubmitController : ApiController
+    {
+        // GET api/submit
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/submit/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/submit
+        public HttpResponseMessage Post([FromBody]string value)
+        {
+            return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+        }
+
+        // PUT api/submit/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/submit/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
